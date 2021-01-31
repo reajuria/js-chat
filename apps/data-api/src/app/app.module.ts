05c9adcd9e05a6c48ec37828ca212307c4e63ab2
Rafael E. Ajuria <reajuria529@gmail.com>
2021-01-30 21:05:20 -0600

    Implement Core Controllers using Redis Repository as Provider

--- apps/data-api/src/app/app.module.ts
+++ apps/data-api/src/app/app.module.ts
@@ -1,7 +1,51 @@
+import { RepositoryCoreControllersModule } from '@js-chat/core-controllers';
+import {
+  RedisConnectionModule,
+  RedisConnectionService,
+} from '@js-chat/redis-connection';
+import {
+  COMMAND_REPOSITORY,
+  CONVERSATION_REPOSITORY,
+  MESSAGE_REPOSITORY,
+  ROOM_REPOSITORY,
+  USER_REPOSITORY,
+} from '@js-chat/repository';
+import {
+  CommandService,
+  ConversationService,
+  MessageService,
+  RoomService,
+  UserService,
+} from '@js-chat/repository-redis';
 import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [],
+  imports: [
+    RedisConnectionModule,
+    RepositoryCoreControllersModule.register([
+      {
+        provide: COMMAND_REPOSITORY,
+        useClass: CommandService,
+      },
+      {
+        provide: CONVERSATION_REPOSITORY,
+        useClass: ConversationService,
+      },
+      {
+        provide: MESSAGE_REPOSITORY,
+        useClass: MessageService,
+      },
+      {
+        provide: ROOM_REPOSITORY,
+        useClass: RoomService,
+      },
+      {
+        provide: USER_REPOSITORY,
+        useClass: UserService,
+      },
+      RedisConnectionService,
+    ]),
+  ],
   controllers: [],
   providers: [],
 })
