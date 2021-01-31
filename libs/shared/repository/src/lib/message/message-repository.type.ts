3133fa69820ab0b17dbeddcbcd3967ce9048d275
Rafael E. Ajuria <reajuria529@gmail.com>
2021-01-30 18:23:02 -0600

    Fix naming

--- libs/shared/repository/src/lib/message/message-repository.type.ts
+++ libs/shared/repository/src/lib/message/message-repository.type.ts
@@ -1,4 +1,4 @@
 import { Message } from '@js-chat/common';
 import { DocumentRepository } from '../document/document-repository.interface';
 
-export type MessageModel = DocumentRepository<Message>;
+export type MessageRepository = DocumentRepository<Message>;
