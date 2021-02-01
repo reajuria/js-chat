64529124893d057a62afbd1531bbd960c0a36a6a
Rafael E. Ajuria <reajuria529@gmail.com>
2021-01-31 22:59:42 -0600

    Add Login

--- apps/js-chat/src/app/services/auth.service.ts
+++ apps/js-chat/src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { User } from '@js-chat/common';
+import { AlertController } from '@ionic/angular';
+import { User, UserData } from '@js-chat/common';
 import { UserClientService } from '@js-chat/ng-api-client';
 import { isNil } from 'lodash';
 import { Subject } from 'rxjs';
@@ -14,19 +15,46 @@ export class AuthService {
 
   user: User;
 
-  constructor(private userService: UserClientService) {
-    this.login('reajuria', '');
+  constructor(
+    private userService: UserClientService,
+    private alertCtrl: AlertController,
+  ) {
+    this.showLogin();
+  }
+
+  async showLogin() {
+    const alert = await this.alertCtrl.create({
+      message: 'Enter a name and username',
+      inputs: [
+        {
+          id: 'name',
+          placeholder: 'Alias',
+          name: 'name',
+        },
+        {
+          id: 'username',
+          placeholder: 'Username',
+          name: 'username',
+        },
+      ],
+      buttons: ['accept'],
+      backdropDismiss: false,
+      keyboardClose: false,
+    });
+    alert.present();
+    const details = (await alert.onDidDismiss())?.data?.values;
+    await this.verifyUser(details);
+  }
+
+  async verifyUser(userData: UserData) {
+    this.user = await this.userService.findOne({ username: userData.username });
+    if (isNil(this.user)) {
+      this.user = await this.userService.insert(userData);
+    }
+    this.login(this.user.username, '');
   }
 
   async login(username: string, password: string) {
-    this.user = await this.userService.findOne({ username });
-    if (isNil(this.user)) {
-      // Evaluation data
-      this.user = await this.userService.insert({
-        alias: 'Rafael',
-        username: 'reajuria',
-      });
-    }
     this.userSubject.next(this.user);
   }
 }
